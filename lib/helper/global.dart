9b32477d480aa34a11a36e7dc77903c65c89bf95
Denis <den.dmitriev.2017@inbox.ru>
2023-01-09 16:38:59 +0300

    nwe

--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -1,5 +1,6 @@
 // ignore: non_constant_identifier_names
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 String? somebodyUid, somebodyFullname, somebodyImageUrl, chatRoomId, GlobalAbout, GlobalAge,GlobalPol,GlobalCity,GlobalRost,GlobalHobbi;
 // ignore: non_constant_identifier_names
@@ -22,3 +23,26 @@ int selectedIndex=1;
 //Filter data
 String FiltrPol="";
 RangeValues currentValues= const RangeValues(18, 22);
+
+
+TextStyle h1_light =
+const TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.w100,
+  fontSize: 32
+);
+
+TextStyle h2_light =
+const TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.w100,
+    fontSize: 22
+);
+
+TextStyle H3 =
+const TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.w200,
+    fontSize: 18
+);
+
--- lib/pages/shop.dart
+++ lib/pages/shop.dart
@@ -28,7 +28,12 @@ class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
 
-    final Podarki=<Podarok>[
+    final podarki=<Podarok>[
+      Podarok(name: 'name', price: 100, img: 'https://bumper-stickers.ru/43054-thickbox_default/podarok.jpg'),
+      Podarok(name: 'name', price: 100, img: 'https://bumper-stickers.ru/43054-thickbox_default/podarok.jpg'),
+      Podarok(name: 'name', price: 100, img: 'https://bumper-stickers.ru/43054-thickbox_default/podarok.jpg'),
+      Podarok(name: 'name', price: 100, img: 'https://bumper-stickers.ru/43054-thickbox_default/podarok.jpg'),
+      Podarok(name: 'name', price: 100, img: 'https://bumper-stickers.ru/43054-thickbox_default/podarok.jpg'),
       Podarok(name: 'name', price: 100, img: 'https://bumper-stickers.ru/43054-thickbox_default/podarok.jpg')
     ];
 
@@ -60,21 +65,69 @@ class _ShopPageState extends State<ShopPage> {
                     minHeight: viewportConstraints.maxHeight,
                   ),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
+
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Text("Подарки"),
-                          Container(
+                          const Text("Подарки",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),textAlign: TextAlign.left,),
+                          SizedBox(
                             height: 200,
                             child: ListView.builder(
                               scrollDirection: Axis.horizontal,
-                              itemBuilder: (BuildContext context, Podarki.length){
-                                return
+                              itemBuilder: (BuildContext context, int index){
+                                return Padding(
+                                  padding: const EdgeInsets.all( 8.0),
+                                  child: SizedBox(
+                                    width: 200,
+                                    child: GestureDetector(
+                                      onTap: (){},
+                                      child: Card(
+                                        elevation: 0,
+                                        shape: RoundedRectangleBorder(
+                                          side: BorderSide(
+                                            color: Theme.of(context).colorScheme.outline,
+                                          ),
+                                          borderRadius: const BorderRadius.all(Radius.circular(12)),
+                                        ),
+                                        color: Colors.white,
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(vertical: 4.0),
+                                          child: Column(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            children: [
+
+                                              SizedBox(
+                                                height: 140,
+                                                  child: Image.network(podarki[index].img,fit: BoxFit.cover,)),
+                                              Padding(
+                                                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                                child: Row(
+                                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                  children: [
+                                                    Text(podarki[index].name,),
+                                                    Row(
+                                                      children: [
+                                                        Text("Стоимость: ${podarki[index].price.toString()} "),
+                                                        const Icon(Icons.monetization_on_outlined),
+                                                      ],
+                                                    )
+                                                  ],
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                );
                               },
-                              itemCount: ,
+                              itemCount: podarki.length,
 
                             ),
-                          )
+                          ),
                         ],
                       )
                     ],
